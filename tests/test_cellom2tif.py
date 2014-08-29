3b758a09f6c7583d7c14e656c594efb8b86a1b8b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2014-08-29 16:33:00 +1000

    Add tests for killing the VM

--- tests/test_cellom2tif.py
+++ tests/test_cellom2tif.py
@@ -3,9 +3,3 @@ from cellom2tif import cellom2tif
 def test_start():
     cellom2tif.start()
     assert cellom2tif.VM_STARTED
-
-
-def test_done():
-    cellom2tif.done()
-    assert cellom2tif.VM_KILLED
-
---
+++ tests/test_zz_jvm_kill.py
@@ -0,0 +1,22 @@
+from cellom2tif import cellom2tif
+import pytest
+
+
+cfile = 'test-data/d1/MFGTMP_120628160001_C18f00d0.C01'
+
+
+def test_read_image():
+    im = cellom2tif.read_image(cfile)
+    assert im.shape == (512, 512)
+
+
+def test_done():
+    cellom2tif.done()
+    assert cellom2tif.VM_KILLED
+
+
+def test_vm_killed_error():
+    cellom2tif.done()
+    with pytest.raises(RuntimeError) as err:
+        cellom2tif.read_image(cfile)
+    assert err.value.message.startswith('The Java Virtual Machine')
