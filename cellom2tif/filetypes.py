dbe7bfdba6392cb2cc5c8d0e710682c2cb9c2bc5
Adrian Hecker <starcalibre@gmail.com>
2015-02-26 09:34:47 +1100

    Add DIB files to cellomics file filter

--- cellom2tif/filetypes.py
+++ cellom2tif/filetypes.py
@@ -1,3 +1,32 @@
+import os
+
+
+def fn_has_ext(fn, ext, case_sensitive=False):
+    """
+    Determine whether a file has a particular extension.
+
+    Parameters
+    ----------
+    fn : string
+        The filename of the query file.
+    ext : string
+        The extension being checked.
+    case_sensitive : bool
+        Whether or not to treat the extension as case sensitive.
+
+    Returns
+    -------
+    file_has_ext : bool
+        True if the filename has the specified extension.
+    """
+    fn_ext = os.path.splitext(fn)[1][1:]
+    if case_sensitive:
+        file_has_ext = fn_ext == ext
+    else:
+        file_has_ext = fn_ext.lower() == ext.lower()
+    return file_has_ext
+
+
 def is_cellomics_image(fn):
     """Determine whether a file is a Cellomics image.
 
@@ -11,7 +40,7 @@ def is_cellomics_image(fn):
     is_cellom : bool
         True if the filename points to a Cellomics image.
     """
-    is_cellom = fn.endswith('.C01') or fn.endswith('.c01')
+    is_cellom = fn_has_ext(fn, 'C01') or fn_has_ext(fn, 'DIB')
     return is_cellom
 
 
