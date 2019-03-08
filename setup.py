c1804d532e7e5a3890635e5edebf377823039dcc
Durand <eric.durand@novartis.com>
2019-03-08 10:53:05 +0100

    install_requires changed to list; travis python version bumped to 3.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ LICENSE             = 'BSD 3-clause'
 DOWNLOAD_URL        = 'https://github.com/jni/cellom2tif'
 VERSION             = '0.3-dev'
 PYTHON_VERSION      = (2, 7)
-INST_DEPENDENCIES   = {} 
+INST_DEPENDENCIES   = []
 
 
 if __name__ == '__main__':
